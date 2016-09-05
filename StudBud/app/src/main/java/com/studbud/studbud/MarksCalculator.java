9f270cf31e55dfbd98137cfc813846d87d2c90e3
Dominik Altmann <dominik.a94@gmx.de>
2016-09-05 12:01:54 +0200

    Startet UI Screens for marks-calculator with expandable list view

--- StudBud/app/src/main/java/com/studbud/studbud/MarksCalculator.java
+++ StudBud/app/src/main/java/com/studbud/studbud/MarksCalculator.java
@@ -7,18 +7,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
+
+import com.studbud.studbud.persistence.ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MarksCalculator extends AppCompatActivity {
 
+    private ExpandableListView informationswissenschaft;
 
     private int subjectID;
     private User user;
     private Calculator calculator;
     public double markOne;
     public double markTwo;
-    private EditText editTextOne;
-    private EditText editTextTwo;
     private Button calculateButton;
     private double[] inf01;
 
@@ -38,9 +44,9 @@ public class MarksCalculator extends AppCompatActivity {
 
 
     private void readUserInput() {
-        checkForEmptyEditText();
-        markOne = Double.parseDouble(editTextOne.getText().toString());
-        markTwo = Double.parseDouble(editTextTwo.getText().toString());
+        //checkForEmptyEditText();
+        /*markOne = Double.parseDouble(editTextOne.getText().toString());
+        markTwo = Double.parseDouble(editTextTwo.getText().toString());*/
         inf01[0] = markOne;
         inf01[1] = markTwo;
 
@@ -48,9 +54,32 @@ public class MarksCalculator extends AppCompatActivity {
 
 
     private void setupUI() {
-        editTextOne = (EditText) findViewById(R.id.mark1);
-        editTextTwo = (EditText) findViewById(R.id.mark2);
         calculateButton = (Button)findViewById(R.id.calculateButton);
+        informationswissenschaft = (ExpandableListView)findViewById(R.id.Informationswissenschaft);
+        List<String> headings = new ArrayList<String>();
+        List<String> L1 = new ArrayList<String>();
+        List<String> L2 = new ArrayList<String>();
+        HashMap<String,List<String>> childList = new HashMap<String,List<String>>();
+        String[] headingItems = getResources().getStringArray(R.array.header_titles);
+        String[] l1 = getResources().getStringArray(R.array.courses_titles);
+        String[] l2 = getResources().getStringArray(R.array.INF_M01);
+
+        for (String title : headingItems){
+            headings.add(title);
+        }
+
+        for (String title : l1){
+            L1.add(title);
+        }
+        for (String title : l2){
+            L2.add(title);
+        }
+
+        childList.put(headings.get(0),L1);
+        childList.put(headings.get(1),L2);
+
+        ExpandableListAdapter adapter = new ExpandableListAdapter(this, headings,childList);
+        informationswissenschaft.setAdapter(adapter);
     }
 
 
@@ -67,14 +96,14 @@ public class MarksCalculator extends AppCompatActivity {
     }
 
 
-    private void checkForEmptyEditText(){
+    /*private void checkForEmptyEditText(){
         if (editTextOne.length() < 1  || editTextTwo.length() < 1){
             editTextOne.setText("4.0");
             editTextTwo.setText("4.0");
             Toast.makeText(MarksCalculator.this, "We calculate the baddest result", Toast.LENGTH_SHORT).show();
         }
 
-    }
+    }*/
 
 
 }
