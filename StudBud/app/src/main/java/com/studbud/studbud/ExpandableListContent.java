d96092604159d6007657ac2f0bc0b0d12ae2021c
Andrul <peterbogensperger@web.de>
2016-09-12 08:21:01 +0200

    Content Class for ExpandableList

---
+++ StudBud/app/src/main/java/com/studbud/studbud/ExpandableListContent.java
@@ -0,0 +1,45 @@
+package com.studbud.studbud;
+
+/**
+ * Created by Der Bar.de on 12.09.2016.
+ */
+/*
+public class ExpandableListContent {
+    private Profile profile;
+    private String medi = "Medieninformatik";
+    private String infw = "Informationswissenschaft";
+    if(profile.getMainSubject().equals(medi))
+
+    {
+        static String[] state = {medi, infw};
+        static String[][] parent = {
+                {"INF-M01", "INF-M02", "INF-M03", "INF-M04", "INF-M05"},
+                {"MEI-M01", "MEI-M01", "MEI-M01", "MEI-M01", "MEI-M01"},
+        };
+
+        static String[][][] child = {
+                {
+                        {"aaa", "aab", "aac", "aad", "aae"},
+                        {"bba", "bbb", "bbc", "bbd", "bbe"},
+                        {"cca", "ccb", "ccc", "ccd", "cce", "ccf", "ccg"},
+                        {"dda", "ddb", "dddc", "ddd", "dde", "ddf"},
+                        {"eea", "eeb", "eec"}
+                },
+                {
+                        {"ffa", "ffb", "ffc", "ffd", "ffe"},
+                        {"gga", "ggb", "ggc", "ggd", "gge"},
+                        {"hha", "hhb", "hhc", "hhd", "hhe", "hhf", "hhg"},
+                        {"iia", "iib", "iic", "iid", "iie", "ii"},
+                        {"jja", "jjb", "jjc", "jjd"}
+                },
+                {
+                        {"kka", "kkb", "kkc", "kkd", "kke"},
+                        {"lla", "llb", "llc", "lld", "lle"},
+                        {"mma", "mmb", "mmc", "mmd", "mme", "mmf", "mmg"},
+                        {"nna", "nnb", "nnc", "nnd", "nne", "nnf"},
+                        {"ooa", "oob"}
+                }
+        };
+    }
+}
+*/
